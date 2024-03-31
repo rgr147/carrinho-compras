543712c8b43ee3c4cdb3707b015beb1eaeba5764
Roger Alves <rgr_147@hotmail.com>
2024-03-31 11:59:07 -0300

    Função limpar() funcionando

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-let valorTotal = 0;
+let valorTotal = 0; //armazena o valor total do carrinho.
 
+//função acionada pela função "adicionar(). Puxa todas as descrições do produto selecionado"
 function pegaProdutoPrecoEscolhido() {
     let descricaoItem = document.getElementById('produto').value; //puxa o item escolhido na lista
     let produto = descricaoItem.split('-')[0].trim();
@@ -8,30 +9,43 @@ function pegaProdutoPrecoEscolhido() {
     return [produto, preco]
 }
 
+// função que identifica a quantia de itens recebida no input da página
 function pegaQuantidadeItens() {
     let quantidade = document.getElementById('quantidade').value;
 
     return parseInt(quantidade);
 }
 
+//funcao de acao do botao "adicionar"
 function adicionar() {
     let itemAdicionado = document.querySelector('.carrinho__produtos__produto');
+    //insere a TAG no carrinho com a descricao e quantidade dos produtos escolhidos
     itemAdicionado.innerHTML += `<br><span class="texto-azul">${pegaQuantidadeItens()}x</span> ${pegaProdutoPrecoEscolhido()[0]} <span class="texto-azul">R$${pegaProdutoPrecoEscolhido()[1]}</span>`;
     calculcarValorCarrinho();
     mostraValorCarrinho();
     // pegaQuantidadeItens();
 }
 
+//função de ação do botão limpar
 function limpar() {
-    document.getElementById('valor-total').textContent = 'R$0'; //limpa valor 'Total'
-    document.getElementById('lista-produtos').textContent = '';  //limpa campo 'Produtos no varrinho'  
+    //zera valor total do carrinho
+    valorTotal = 0;
+    mostraValorCarrinho();
+
+    //subsitui itens do carrinho por string vazia
+    let itemAdicionado = document.querySelector('.carrinho__produtos__produto');
+    itemAdicionado.innerHTML = '';
 }
 
+//função responsável por calcular o valor total do carrinho
 function calculcarValorCarrinho() {
-    valorTotal = valorTotal + (pegaQuantidadeItens() * pegaProdutoPrecoEscolhido()[1])
+    valorTotal = valorTotal + (pegaQuantidadeItens() * pegaProdutoPrecoEscolhido()[1]) //soma a variavel "valorTotal" com os valores da função "pegaProdutoPrecoEscolhido()"
 }
 
+//função que atualiza o valor total do carrinho na página
 function mostraValorCarrinho() {
-    let valorCarrinho = document.getElementById('valor-total');
+    let valorCarrinho = document.getElementById('valor-total'); //identifica o caminho da classe que mostra o valor do carrinho
     valorCarrinho.textContent = `R$${valorTotal}`;
 }
+
+limpar();
